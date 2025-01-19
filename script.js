e138b41b4a18074b69e5747ce0bd11b41fec0712
Zabsooon <jakub.zabski@protonmail.com>
2025-01-19 23:36:15 +0100

    Add drawing feature

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+const container = document.querySelector("#container");
+
+function createGrid(height, width) {
+    for(let i = 0; i < height; i++) {
+        let row = document.createElement("div");
+        row.classList.toggle('row');
+        for(let j = 0; j < width; j++) {
+            let innerSquare = document.createElement("div");
+            innerSquare.classList.toggle('square')
+            innerSquare.style.width = `${640 / width}px`;
+            innerSquare.style.height = `${640 / height}px`;
+            innerSquare.addEventListener("mouseover", () => {
+                innerSquare.style.backgroundColor = "#ffff33";
+            });
+            row.appendChild(innerSquare);
+        }
+        container.appendChild(row);
+    }   
+}
+
+createGrid(16, 16);
