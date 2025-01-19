daa2f3244cb06271f4f74fa9d7b30b30f6239f1b
Zabsooon <jakub.zabski@protonmail.com>
2025-01-19 17:56:50 +0100

    Add basic interactive UI and change the code to work with UI

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('Hello World!');
-
 function getComputerChoice() {
     let rand = Math.random();
     if(rand >= 0.66) {
@@ -11,46 +9,71 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Enter your choice (rock, paper, scissors):");
-}
-
 function playRound(humanChoice, computerChoice) {
+    // Reset the game
+    if(humanScore == 5 || computerScore == 5) {
+        humanScore = 0;
+        computerScore = 0;
+    }
     computerChoice = computerChoice.toLowerCase()
     humanChoice = humanChoice .toLowerCase();
 
     if(computerChoice == humanChoice) {
-        return; // draw
+        winningTitle.innerText = 'Draw!';
     }
     if(computerChoice == 'scissors' && humanChoice == 'rock') {
         humanScore++;
+        winningTitle.innerText = 'Player wins this round!';
     }
     if(computerChoice == 'rock' && humanChoice == 'scissors') {
         computerScore++;
+        winningTitle.innerText = 'Computer wins this round!';
     }
     if(computerChoice == 'paper' && humanChoice == 'scissors') {
         humanScore++;
+        winningTitle.innerText = 'Player wins this round!';
     }
     if(computerChoice == 'scissors' && humanChoice == 'paper') {
         computerScore++;
+        winningTitle.innerText = 'Computer wins this round!';
     }
     if(computerChoice == 'rock' && humanChoice == 'paper') {
         humanScore++;
+        winningTitle.innerText = 'Player wins this round!';
     }
     if(computerChoice == 'paper' && humanChoice == 'rock') {
         computerScore++;
+        winningTitle.innerText = 'Computer wins this round!';
     }
+
+    console.log(`Human: ${humanChoice} Computer: ${computerChoice}`);
+    if(humanScore >= 5) {
+        winningTitle.innerText = 'Player wins the game!';
+    } else if(computerScore >= 5) {
+        winningTitle.innerText = 'Computer wins the game!';
+    }
+    humanScoreDiv.innerText = humanScore;
+    computerScoreDiv.innerText = computerScore;
 }
 
-function playGame() {
-    // You play until someone gets 5 points
-    while(humanScore < 5 || computerScore < 5) {
-        playRound(getHumanChoice(), getComputerChoice());
-
-        console.log(computerSelection)
-        console.log('Human score: ' + humanScore)
-        console.log('Computer score: ' + computerScore)
-    }
-}
+const buttons = document.querySelectorAll("button");
 
 let humanScore = 0, computerScore = 0;
+for(const button of buttons) {
+    button.addEventListener("click", (e) => {
+        console.log(e.target.id);
+        playRound(e.target.id, getComputerChoice());
+    });
+}
+
+const scoreDiv = document.querySelector("#score")
+
+const winningTitle = document.createElement("p");
+const humanScoreDiv = document.createElement("div");
+const computerScoreDiv = document.createElement("div");
+humanScoreDiv.classList.add("score_div");
+computerScoreDiv.classList.add("score_div");
+
+scoreDiv.appendChild(winningTitle);
+scoreDiv.appendChild(humanScoreDiv);
+scoreDiv.appendChild(computerScoreDiv);
