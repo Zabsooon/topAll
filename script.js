14d8307a5d4d13fee4a6c091a4df0a49d57f10c8
Zabsooon <jakub.zabski@protonmail.com>
2025-01-23 13:56:27 +0000

    Add styling and basic operation functions

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('Hello World!');
+function add(left, right) {
+    return left + right;
+}
+
+function subtract(left, right) {
+    return left - right;
+}
+
+function multiply(left, right) {
+    return left * right;
+}
+
+function divide(left, right) {
+    return left / right;
+}
+
+function operate(operator, left, right) {
+    switch(operator) {
+        case '+':
+            return add(left, right);
+        case '-':
+            return subtract(left, right);
+        case '*':
+            return multiply(left, right);
+        case '/':
+            return divide(left, right);
+    }
+}
+
+
+let leftNumber = undefined;
+let rightNumber = undefined;
+let operator = undefined;
