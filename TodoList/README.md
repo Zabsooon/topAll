2fe00e56042bd4226103da3fd2ae6a6c87a17f2b
Zabsooon <jakub.zabski@protonmail.com>
2025-02-25 21:04:40 +0100

    Add todo creation functionality with updates in TodoList project(semi-functional)

---
+++ TodoList/README.md
@@ -0,0 +1,10 @@
+# TodoList
+
+Well... this is tragic. I honestly would re-write all off this, because it shouldn't even work,
+design patterns used? MAGIC! ON A GOD DAMN EDGE OF RECURSION...
+
+I could make it look good but for what? It shouldn't even work in the first place.
+Im abondoning this project and I am moving forward to another one, since approach
+to this one was wrong at the start...
+
+C/C++ better ;3
--- TodoList/src/dom_project.js
+++ TodoList/src/dom_project.js
@@ -1,100 +1,251 @@
 import * as projectCss from './project.css'
 import * as todoCss from './todo.css'
+import { Project } from './project';
+import Todo from './todo.js'
 
 export let state = {
-  selectedProjectIndex: -1,
-  projects: [],
+    selectedProjectIndex: -1,
+    projects: [],
+    isRefreshing: false,
 }
 
+
+/*
+ * This function creates objects and apply them to the html,
+ * therefore they are being generated on the page.
+ */
+function buildLeftPageDOM() {
+    let leftDiv = document.querySelector('#left');
+    leftDiv.innerHTML = ''; // Prevent duplication
+
+    let createProjectDialog = document.createElement('dialog');
+    createProjectDialog.setAttribute('id', 'createProjectDialog');
+
+    let createProjectBtn = document.createElement('button'); // Shows the dialog
+    createProjectBtn.innerText = 'Create New Project';
+
+    createProjectBtn.addEventListener('click', () => {
+        console.log("Opening project creation dialog"); // Debugging
+        createProjectDialog.showModal();
+    });
+
+    let createProjectForm = document.createElement('form');
+    createProjectForm.setAttribute('method', 'dialog');
+
+    let createProjectNameLabel = document.createElement('label');
+    createProjectNameLabel.innerText = 'Project Name';
+    let createProjectNameInput = document.createElement('input');
+    createProjectNameInput.setAttribute('id', 'projectName');
+
+    let createProjectSubmitBtn = document.createElement('button');
+    createProjectSubmitBtn.innerText = 'Create';
+
+    createProjectSubmitBtn.addEventListener('click', (e) => {
+        e.preventDefault(); // Prevent immediate form close
+
+        let name = createProjectNameInput.value.trim();
+        if (!name) {
+            alert("Project name cannot be empty.");
+            return;
+        }
+        let newProject = new Project(state.projects.length, name, 'default-icon');
+        state.projects.push(newProject);
+        console.log("Created project:", newProject.name);
+
+        createProjectDOM(newProject); // Add to DOM
+        rightRefreshDOM(); // Update right panel
+        createProjectDialog.close();
+    });
+
+    createProjectForm.appendChild(createProjectNameLabel);
+    createProjectForm.appendChild(createProjectNameInput);
+    createProjectForm.appendChild(createProjectSubmitBtn);
+    createProjectDialog.appendChild(createProjectForm);
+
+    leftDiv.appendChild(createProjectDialog);
+    leftDiv.appendChild(createProjectBtn);
+}
+
+
+function buildRightPageDOM() {
+
+    let rightDiv = document.querySelector('#right');
+    rightDiv.innerHTML = ''
+
+    let todosDiv = document.createElement('div');
+    todosDiv.id = 'todosDiv';
+    rightDiv.appendChild(todosDiv);
+
+    let createTodoDialog = document.createElement('dialog');
+    let createTodoBtn = document.createElement('button'); /* Shows dialog with create todo button */
+    createTodoBtn.innerText = 'Create New Todo';
+
+    createTodoBtn.addEventListener('click', () => {
+        createTodoDialog.showModal();
+    });
+
+    let createTodoForm = document.createElement('form');
+    createTodoForm.setAttribute('method', 'dialog');
+
+    let createTodoElements = ['Title', 'Description', 'DueDate', 'Priority', 'Status'];
+    for (let element of createTodoElements) {
+        let tempLabel = document.createElement('label');
+        tempLabel.setAttribute('for', `todo${element}`);
+        tempLabel.innerText = `Todo ${element}`;
+        let tempInput = document.createElement('input');
+        tempInput.setAttribute('name', `todo${element}`);
+        tempInput.setAttribute('id', `todo${element}`);
+
+
+        createTodoForm.appendChild(tempLabel);
+        createTodoForm.appendChild(tempInput);
+    }
+
+    let createTodoSubmitBtn = document.createElement('button');
+    createTodoSubmitBtn.setAttribute('type', 'submit');
+    createTodoSubmitBtn.innerText = 'Create';
+
+    createTodoForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        if (state.selectedProjectIndex === -1 || state.selectedProjectIndex >= state.projects.length) {
+            alert('You have to choose project');
+            return;
+        }
+        let newTodoTitle = document.querySelector('#todoTitle').value.trim();
+        let newTodoDescription = document.querySelector('#todoDescription').value.trim();
+        let newTodoDueDate = document.querySelector('#todoDueDate').value.trim();
+        let newTodoPriority = document.querySelector('#todoPriority').value.trim();
+        let newTodoStatus = document.querySelector('#todoStatus').value.trim();
+
+        console.log(newTodoTitle, newTodoDescription, newTodoDueDate, newTodoPriority, newTodoStatus);
+
+        if (!newTodoTitle || !newTodoDescription || !newTodoDueDate || !newTodoPriority || !newTodoStatus) {
+            alert('All fields must be set');
+            return;
+        }
+
+        let newTodo = new Todo(newTodoTitle,
+            newTodoDescription,
+            newTodoDueDate,
+            newTodoPriority,
+            newTodoStatus);
+        state.projects.at(state.selectedProjectIndex).todo_list.push(newTodo);
+        state.isRefreshing = false;
+        rightRefreshDOM();
+        createTodoDialog.close();
+    });
+
+    createTodoForm.appendChild(createTodoSubmitBtn);
+    createTodoDialog.appendChild(createTodoForm);
+
+    rightDiv.appendChild(createTodoDialog);
+    rightDiv.appendChild(createTodoBtn);
+}
+
+
 function leftRefreshDOM() {
-  let leftDiv = document.querySelector('#left');
+    if(state.isRefreshing) return;
+    state.isRefreshing = true;
+    let leftDiv = document.querySelector('#left');
 
-  let createProjectDialog = leftDiv.querySelector('dialog');
-  let createProjectBtn = leftDiv.querySelector('button');
+    let createProjectDialog = leftDiv.querySelector('dialog');
+    let createProjectBtn = leftDiv.querySelector('button');
 
-  leftDiv.innerHTML = '';
+    leftDiv.innerHTML = '';
 
-  if (createProjectDialog) leftDiv.appendChild(createProjectDialog);
-  if (createProjectBtn) leftDiv.appendChild(createProjectBtn);
+    if (createProjectDialog) leftDiv.appendChild(createProjectDialog);
+    if (createProjectBtn) leftDiv.appendChild(createProjectBtn);
 
-  for(let project of state.projects) {
-    createProjectDOM(project);
-  }
+    for (let project of state.projects) {
+        state.isRefreshing = false;
+        createProjectDOM(project);
+    }
+
+    state.isRefreshing = false;
 }
 
 function rightRefreshDOM() {
-  let rightDiv = document.querySelector('#right');
+    if(state.isRefreshing) return;
+    state.isRefreshing = true;
+    let todosDiv = document.querySelector('#todosDiv');
+    todosDiv.innerHTML = ''; // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  let createTodoDialog = rightDiv.querySelector('dialog');
-  let createTodoBtn = rightDiv.querySelector('button');
+    if (state.selectedProjectIndex < 0 || state.selectedProjectIndex >= state.projects.length) {
+        console.warn('No valid project selected');
+        return;
+    }
 
-  rightDiv.querySelectorAll('todo').forEach(todo => todo.remove());
+    for (let todo of state.projects[state.selectedProjectIndex].todo_list) {
+        state.isRefreshing = false;
+        createTodoDOM(todo);
+    }
 
-  if(state.selectedProjectIndex < 0 || state.selectedProjectIndex >= state.projects.length) {
-    console.warn('No valid project selected');
-    return;
-  }
-
-  for(let todo of state.projects[state.selectedProjectIndex].todo_list) {
-    createTodoDOM(todo);
-  }
-
-  if(!rightDiv.contains(createTodoDialog)) rightDiv.appendChild(createTodoDialog);
-  if(!rightDiv.contains(createTodoBtn)) rightDiv.appendChild(createTodoBtn);
+    state.isRefreshing = false;
 }
 
 function createProjectDOM(project) {
-  let projectDiv = document.createElement('div');
-  projectDiv.className = 'project';
-  
-  let projectIcon = document.createElement('h5');
-  projectIcon.className = project.icon;
-  let projectName = document.createElement('h5');
-  projectName.innerText = project.name;
+    if(state.isRefreshing) return;
+    state.isRefreshing = true;
 
-  projectDiv.appendChild(projectIcon);
-  projectDiv.appendChild(projectName);
+    let projectDiv = document.createElement('div');
+    projectDiv.className = 'project';
 
-  projectDiv.addEventListener('click', () => {
-    state.selectedProjectIndex = state.projects.findIndex(p => p.name === project.name);
-    console.log("Project selected:", state.selectedProjectIndex, project.name); // Debug
-    rightRefreshDOM(); 
-  });
+    let projectIcon = document.createElement('h5');
+    projectIcon.className = project.icon;
+    let projectName = document.createElement('h5');
+    projectName.innerText = project.name;
 
-  let leftDiv = document.querySelector('#left');
-  leftDiv.appendChild(projectDiv);
+    projectDiv.appendChild(projectIcon);
+    projectDiv.appendChild(projectName);
+
+    projectDiv.addEventListener('click', () => {
+        state.selectedProjectIndex = project.id;
+        console.log("Project selected:", state.selectedProjectIndex, project.name); // Debug
+        state.isRefreshing = false;
+        rightRefreshDOM();
+    });
+
+    let leftDiv = document.querySelector('#left');
+    leftDiv.appendChild(projectDiv);
+
+    state.isRefreshing = false;
 }
 
 
 function createTodoDOM(todo) {
-  let todoDiv = document.createElement('div');
-  todoDiv.className = 'todo';
-  
-  let todoTitle = document.createElement('h3');
-  todoTitle.className = 'todoTitle';
-  todoTitle.innerText = todo.title;
-  let todoDescription = document.createElement('p');
-  todoDescription.className = 'todoDescription';
-  todoDescription.innerText = todo.description;
-  let todoDueDate = document.createElement('p');
-  todoDueDate.className = 'todoDueDate';
-  todoDueDate.innerText = todo.dueDate;
-  let todoPriority = document.createElement('p');
-  todoPriority.className = 'todoPriority';
-  todoPriority.innerText = todo.priority;
-  let todoStatus = document.createElement('p');
-  todoStatus.className = 'todoStatus';
-  todoStatus.innerText = todo.status;
+    if(state.isRefreshing) return;
+    state.isRefreshing = true;
 
-  todoDiv.appendChild(todoTitle);
-  todoDiv.appendChild(todoDescription);
-  todoDiv.appendChild(todoDueDate);
-  todoDiv.appendChild(todoPriority);
-  todoDiv.appendChild(todoStatus);
+    let todoDiv = document.createElement('div');
+    todoDiv.className = 'todo';
 
-  let rightDiv = document.querySelector('#right');
-  rightDiv.appendChild(todoDiv);
-  rightRefreshDOM();
+    let todoTitle = document.createElement('h3');
+    todoTitle.className = 'todoTitle';
+    todoTitle.innerText = `Title: ${todo.title}`;
+    let todoDescription = document.createElement('p');
+    todoDescription.className = 'todoDescription';
+    todoDescription.innerText = `Description: ${todo.description}`;
+    let todoDueDate = document.createElement('p');
+    todoDueDate.className = 'todoDueDate';
+    todoDueDate.innerText = `Due date: ${todo.dueDate}`;
+    let todoPriority = document.createElement('p');
+    todoPriority.className = 'todoPriority';
+    todoPriority.innerText = `Priority: ${todo.priority}`;
+    let todoStatus = document.createElement('p');
+    todoStatus.className = 'todoStatus';
+    todoStatus.innerText = `Status ${todo.status}`;
+
+    todoDiv.appendChild(todoTitle);
+    todoDiv.appendChild(todoDescription);
+    todoDiv.appendChild(todoDueDate);
+    todoDiv.appendChild(todoPriority);
+    todoDiv.appendChild(todoStatus);
+
+    let todosDiv = document.querySelector('#todosDiv');
+    todosDiv.appendChild(todoDiv);
+
+    state.isRefreshing = false;
 }
 
-export { createTodoDOM, createProjectDOM, leftRefreshDOM, rightRefreshDOM };
+export { buildLeftPageDOM, buildRightPageDOM, createTodoDOM, createProjectDOM, leftRefreshDOM, rightRefreshDOM };
