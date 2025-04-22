1d9908be3f58b50dfd141fa51c71141cac6ed6bd
Zabsooon <jakub.zabski@protonmail.com>
2025-04-22 19:31:59 +0200

    Test basic LinkedList functionality

--- LinkedList/src/index.js
+++ LinkedList/src/index.js
@@ -1,1 +1,17 @@
 import './styles.css'
+import LinkedList from './LinkedList.js'
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+const list2 = new LinkedList();
+
+console.log(list.toString());
+
+console.log(list2.toString());
